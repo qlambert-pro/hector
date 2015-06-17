22be35219913bb47cfcf2719b2f64813ed0050f8
Quentin Lambert <lambert.quentin@gmail.com>
2015-06-17 11:50:07 +0200

    fixing indentation and minor rewriting of analyzer.ml and main.ml

--- analyzer/analyzer.ml
+++ analyzer/analyzer.ml
@@ -25,31 +25,31 @@ let all_def = ref []
 
 
 let rec create_rr_ops_list func_name iifunc1 lbl_list rr_ops_list init_lbl_list errblks_list = function
-  []-> rr_ops_list 
-      
+    []-> rr_ops_list 
+
   | (brnch_strtlineno,test_case,typ,blk_strtlineno,blk_endlineno,stmtlist)::t->
-      let new_rr_ops_list= Rr_op_finder.stack_rr_op_new func_name iifunc1 lbl_list rr_ops_list [] stmtlist stmtlist in
-      create_rr_ops_list func_name iifunc1 lbl_list new_rr_ops_list init_lbl_list errblks_list t
+    let new_rr_ops_list= Rr_op_finder.stack_rr_op_new func_name iifunc1 lbl_list rr_ops_list [] stmtlist stmtlist in
+    create_rr_ops_list func_name iifunc1 lbl_list new_rr_ops_list init_lbl_list errblks_list t
 
 
 
 let rec recheck_blks func_name iifunc1  lbl_list prog = function
-       []-> []
-     | (brnch_strtlineno,test_case,goto,st,typ,blk_strtlineno,blk_endlineno,stmtlist)::t->
-           if not (Errorhandling.exists_error_handling_block test_case lbl_list
+    []-> []
+  | (brnch_strtlineno,test_case,goto,st,typ,blk_strtlineno,blk_endlineno,stmtlist)::t->
+    if not (Errorhandling.exists_error_handling_block test_case lbl_list
               prog stmtlist typ blk_strtlineno)
-	      then((recheck_blks func_name iifunc1 lbl_list  prog t))
-           else (brnch_strtlineno,test_case,goto,st,typ,blk_strtlineno,blk_endlineno,stmtlist)::(recheck_blks func_name iifunc1 lbl_list  prog t)
+    then((recheck_blks func_name iifunc1 lbl_list  prog t))
+    else (brnch_strtlineno,test_case,goto,st,typ,blk_strtlineno,blk_endlineno,stmtlist)::(recheck_blks func_name iifunc1 lbl_list  prog t)
 
 let rec refine_rr_ops_inner args_list = function
-  []-> false
+    []-> false
   | (alloc, args, rrl)::t-> 
-         if (Def.compare_explists args args_list) then true
-         else refine_rr_ops_inner args_list t
+    if (Def.compare_explists args args_list) then true
+    else refine_rr_ops_inner args_list t
 
 
 let rec refine_rr_ops = function
-  []->[]
+    []->[]
   | (alloc, args, rrl)::t->
     if (refine_rr_ops_inner args t) then refine_rr_ops t
     else (alloc, args, rrl)::(refine_rr_ops t)
@@ -57,51 +57,51 @@ let rec refine_rr_ops = function
 
 let analyze_each_blk_new func lbl_list rr_ops_list prog iifunc1 all_def func_name init_lbl_list errblks_list =
   let rec analyze_each_blk_loop_new  = function
-    []->[]
+      []->[]
     |  (brnch_strtlineno,test_case,typ,blk_strtlineno,blk_endlineno,stmtlist)::t-> 
-          let miss_rr_ops_list = Rr_op_finder.find_missing_rr_ops_new lbl_list stmtlist rr_ops_list in
-          let update_miss_rr_ops_list = refine_rr_ops miss_rr_ops_list in
-                 if(List.length update_miss_rr_ops_list)> 0 then(
-                      let miss_rr_ops_list_new  = Rm_true_positives.is_resource_having_same_def_new func_name iifunc1 []
-                          blk_strtlineno lbl_list prog errblks_list init_lbl_list update_miss_rr_ops_list  in
-                            if(List.length miss_rr_ops_list_new)> 0 then(
-                              let miss_rr_ops_list1 = Rm_true_positives.is_rrwa_alloc errblks_list stmtlist 
-                                                                 blk_strtlineno [] lbl_list prog miss_rr_ops_list_new  in
-                                if(List.length miss_rr_ops_list1)> 0 then(
-                                  let miss_rr_ops_list_new2  = Rm_true_positives.return_resource_new lbl_list stmtlist []  miss_rr_ops_list1 in
-                                    if(List.length miss_rr_ops_list_new2)> 0 then(
-                                      let miss_rr_ops_list_new3 = 
-                                           Rm_true_positives.rls_in_exe_paths blk_strtlineno lbl_list prog errblks_list test_case 
-					       stmtlist init_lbl_list [] miss_rr_ops_list_new2 in
-                                         if(List.length miss_rr_ops_list_new3)> 0 then(
-                                           let miss_rr_ops_list_new4 =  
-                                              Interproc.find_interproc_calls stmtlist all_def  blk_strtlineno lbl_list prog [] miss_rr_ops_list_new3 in  
-  					   let miss_rr_ops_list_new5 =  
-                                            Interproc.interproc_new func all_def blk_strtlineno errblks_list prog lbl_list  [] miss_rr_ops_list_new4 in  
-                                           let miss_rr_ops_list_new6 =  
-					    Rm_true_positives.resource_is_not_allocated_yet errblks_list blk_strtlineno prog lbl_list [] miss_rr_ops_list_new5 in  
-                                              if(List.length miss_rr_ops_list_new6)> 0 then(
-                                                Report.generate_report_new func_name blk_strtlineno  iifunc1 miss_rr_ops_list_new6;
+      let miss_rr_ops_list = Rr_op_finder.find_missing_rr_ops_new lbl_list stmtlist rr_ops_list in
+      let update_miss_rr_ops_list = refine_rr_ops miss_rr_ops_list in
+      if(List.length update_miss_rr_ops_list)> 0 then(
+        let miss_rr_ops_list_new  = Rm_true_positives.is_resource_having_same_def_new func_name iifunc1 []
+            blk_strtlineno lbl_list prog errblks_list init_lbl_list update_miss_rr_ops_list  in
+        if(List.length miss_rr_ops_list_new)> 0 then(
+          let miss_rr_ops_list1 = Rm_true_positives.is_rrwa_alloc errblks_list stmtlist 
+              blk_strtlineno [] lbl_list prog miss_rr_ops_list_new  in
+          if(List.length miss_rr_ops_list1)> 0 then(
+            let miss_rr_ops_list_new2  = Rm_true_positives.return_resource_new lbl_list stmtlist []  miss_rr_ops_list1 in
+            if(List.length miss_rr_ops_list_new2)> 0 then(
+              let miss_rr_ops_list_new3 = 
+                Rm_true_positives.rls_in_exe_paths blk_strtlineno lbl_list prog errblks_list test_case 
+                  stmtlist init_lbl_list [] miss_rr_ops_list_new2 in
+              if(List.length miss_rr_ops_list_new3)> 0 then(
+                let miss_rr_ops_list_new4 =  
+                  Interproc.find_interproc_calls stmtlist all_def  blk_strtlineno lbl_list prog [] miss_rr_ops_list_new3 in  
+                let miss_rr_ops_list_new5 =  
+                  Interproc.interproc_new func all_def blk_strtlineno errblks_list prog lbl_list  [] miss_rr_ops_list_new4 in  
+                let miss_rr_ops_list_new6 =  
+                  Rm_true_positives.resource_is_not_allocated_yet errblks_list blk_strtlineno prog lbl_list [] miss_rr_ops_list_new5 in  
+                if(List.length miss_rr_ops_list_new6)> 0 then(
+                  Report.generate_report_new func_name blk_strtlineno  iifunc1 miss_rr_ops_list_new6;
 
-                                               analyze_each_blk_loop_new t
-                                          )
-                                          else analyze_each_blk_loop_new t
-                                    )
-                                    else analyze_each_blk_loop_new t
-                                 )
-                                 else analyze_each_blk_loop_new t
-                          )
-                          else analyze_each_blk_loop_new t
-                       )
-                       else analyze_each_blk_loop_new t
-                 )
-                 else analyze_each_blk_loop_new t
-   in analyze_each_blk_loop_new errblks_list
-
-
+                  analyze_each_blk_loop_new t
+                )
+                else analyze_each_blk_loop_new t
+              )
+              else analyze_each_blk_loop_new t
+            )
+            else analyze_each_blk_loop_new t
+          )
+          else analyze_each_blk_loop_new t
+        )
+        else analyze_each_blk_loop_new t
+      )
+      else analyze_each_blk_loop_new t
+  in analyze_each_blk_loop_new errblks_list
 
 
-         
+
+
+
 
 
 
@@ -110,95 +110,75 @@ let analyze_each_blk_new func lbl_list rr_ops_list prog iifunc1 all_def func_nam
 let rec create_init_lbl_prog_list prog_list init_lbl_list  = function
     []-> (prog_list,init_lbl_list)
   |  h::t-> (match Ast_c.unwrap h with
-              Ast_c.Labeled (Ast_c.Label (name, st)) ->if (List.length init_lbl_list)>0 then
-                                                          (match Ast_c.unwrap st with
-                                                          |      Ast_c.ExprStatement (Some e) ->
-                                                                 create_init_lbl_prog_list prog_list (init_lbl_list@[st]) t
-                                                          |      Ast_c.Jump (Ast_c.Goto name) -> (prog_list, (init_lbl_list@[st]))
-                                                          |      Ast_c.Jump (Ast_c.Return) -> (prog_list, (init_lbl_list@[st]))
-                                                          |      Ast_c.Jump (Ast_c.ReturnExpr e) -> (prog_list, (init_lbl_list@[st]))
-                                                          |      Ast_c.Decl decl -> create_init_lbl_prog_list prog_list (init_lbl_list@[st]) t
-                                                          |      _ -> (prog_list,[])
-                                                          )
-                                                       else (prog_list,init_lbl_list)
+        Ast_c.Labeled (Ast_c.Label (name, st)) ->if (List.length init_lbl_list)>0 then
+          (match Ast_c.unwrap st with
+           |      Ast_c.ExprStatement (Some e) ->
+             create_init_lbl_prog_list prog_list (init_lbl_list@[st]) t
+           |      Ast_c.Jump (Ast_c.Goto name) -> (prog_list, (init_lbl_list@[st]))
+           |      Ast_c.Jump (Ast_c.Return) -> (prog_list, (init_lbl_list@[st]))
+           |      Ast_c.Jump (Ast_c.ReturnExpr e) -> (prog_list, (init_lbl_list@[st]))
+           |      Ast_c.Decl decl -> create_init_lbl_prog_list prog_list (init_lbl_list@[st]) t
+           |      _ -> (prog_list,[])
+          )
+        else (prog_list,init_lbl_list)
 
-    |  Ast_c.ExprStatement (Some e) -> create_init_lbl_prog_list prog_list (init_lbl_list@[h]) t
-    |  Ast_c.Jump (Ast_c.Goto name) -> (prog_list, (init_lbl_list@[h]))
-    |  Ast_c.Jump (Ast_c.Return) -> (prog_list, (init_lbl_list@[h]))
-    |  Ast_c.Jump (Ast_c.ReturnExpr e) -> (prog_list, (init_lbl_list@[h]))
-    |  Ast_c.Decl decl -> create_init_lbl_prog_list prog_list (init_lbl_list@[h]) t
-    |  _ -> create_init_lbl_prog_list (prog_list@init_lbl_list@[h]) [] t
-  )
+      |  Ast_c.ExprStatement (Some e) -> create_init_lbl_prog_list prog_list (init_lbl_list@[h]) t
+      |  Ast_c.Jump (Ast_c.Goto name) -> (prog_list, (init_lbl_list@[h]))
+      |  Ast_c.Jump (Ast_c.Return) -> (prog_list, (init_lbl_list@[h]))
+      |  Ast_c.Jump (Ast_c.ReturnExpr e) -> (prog_list, (init_lbl_list@[h]))
+      |  Ast_c.Decl decl -> create_init_lbl_prog_list prog_list (init_lbl_list@[h]) t
+      |  _ -> create_init_lbl_prog_list (prog_list@init_lbl_list@[h]) [] t
+    )
 
+let analyze_def full_file all_fn (defbis,
+                                  iifunc1::iifunc2::i1::i2::ifakestart::isto) =
+  let {Ast_c.f_name = name;
+       Ast_c.f_body = statxs;
+      } = defbis
+  in
+  let lbl_list = Def.create_lbl_list [] [] (Def.remove_stmtElelist statxs) in
+  let s = fst (Ast_c.get_s_and_ii_of_name name) in
+  Var_dec.func_name := s;
+  Var_dec.file_info := Ast_c.file_of_info iifunc1;
+  let prog = (Def.remove_stmtElelist statxs) in
+  let (prog_list, init_lbl_list) = create_init_lbl_prog_list [] [] prog in
+  let new_errblks_list = Errorhandling.find_errorhandling lbl_list prog in
+  let updated_blk_list =
+    recheck_blks s iifunc1 lbl_list prog new_errblks_list in
+  let updated_blk_list =
+    Rm_true_positives.remove_blks_that_returns_resource prog updated_blk_list updated_blk_list in
 
+  let (updated_blk_list, f_basic, f_goto) =
+    let rec loop f_basic f_goto list = function
+        [] -> (list, f_basic, f_goto)
+      | (brnch_strtlineno,test_case,goto,st_normal,typ,blk_strtlineno,blk_endlineno,stmtlist)::t->
+        match goto with
+          Some a -> loop f_basic true   ((brnch_strtlineno, test_case,(* mender :  st_normal, *)typ, blk_strtlineno, blk_endlineno, stmtlist)::list) t
+        |	_      -> loop true    f_goto ((brnch_strtlineno, test_case,(* mender :  st_normal, *)typ, blk_strtlineno, blk_endlineno, stmtlist)::list) t
+    in loop false false [] updated_blk_list
+  in
 
+  let rr_ops_list_new =
+    match updated_blk_list with
+      h1::h2::t -> create_rr_ops_list s iifunc1 lbl_list [] init_lbl_list updated_blk_list updated_blk_list
+    | _         -> []
+  in
 
- 
-let  analyze_def full_file all_fn def =
-    let defbis, ii = def in
-    match ii with
-    |    iifunc1::iifunc2::i1::i2::ifakestart::isto ->
-        let {Ast_c.f_name = name;
-             Ast_c.f_type = (returnt, (paramst, (b, iib)));
-             Ast_c.f_storage = sto;
-             Ast_c.f_body = statxs;
-             Ast_c.f_attr = attrs;
-            }    = defbis
-            in
-            let lbl_list = Def.create_lbl_list [] [] (Def.remove_stmtElelist statxs) in
+  let tmplist =
+    match (updated_blk_list, rr_ops_list_new) with
+      (h1::h2::t, l::u) -> analyze_each_blk_new name lbl_list rr_ops_list_new prog iifunc1 !all_def s init_lbl_list updated_blk_list
+    | _                 -> []
+  in
+  ({defbis with Ast_c.f_body = []},
+   iifunc1::iifunc2::i1::i2::ifakestart::isto)
 
-         let (s,inf) = Ast_c.get_s_and_ii_of_name name in
-         Var_dec.func_name:=s;
-         Var_dec.file_info:=Ast_c.file_of_info iifunc1;
-
-         let (prog_list,init_lbl_list)= create_init_lbl_prog_list [] [] (Def.remove_stmtElelist statxs)  in
-         let prog = (Def.remove_stmtElelist statxs) in
-        
-         
-         let new_errblks_list = Errorhandling.find_errorhandling lbl_list prog  in 
-        
-         let updated_blk_list = recheck_blks s iifunc1 lbl_list prog new_errblks_list in
-         let updated_blk_list = Rm_true_positives.remove_blks_that_returns_resource prog updated_blk_list updated_blk_list in
-
-
-         let (updated_blk_list, f_basic, f_goto) =
-           let rec loop f_basic f_goto list = function
-             []-> (list, f_basic, f_goto)
-             |  (brnch_strtlineno,test_case,goto,st_normal,typ,blk_strtlineno,blk_endlineno,stmtlist)::t->
-                      match goto with
-                      |	 Some a -> loop f_basic true ((brnch_strtlineno,test_case,(* mender :  st_normal, *)typ,blk_strtlineno,blk_endlineno,stmtlist)::list) t
-                      |	 _-> loop true f_goto ((brnch_strtlineno,test_case,(* mender :  st_normal, *) typ,blk_strtlineno,blk_endlineno,stmtlist)::list) t
-           in loop false false [] updated_blk_list
-         in
-
-
-      
-         let rr_ops_list_new = if (List.length updated_blk_list)>1 then (  
-	                         create_rr_ops_list s iifunc1 lbl_list [] init_lbl_list updated_blk_list updated_blk_list ) 
-	                       else [] in   
-
-
-        let tmplist = 
-           if (List.length updated_blk_list)>  1  && (List.length rr_ops_list_new)>0 then  (
-             analyze_each_blk_new name lbl_list rr_ops_list_new prog iifunc1 !all_def s init_lbl_list updated_blk_list )
-  	   else []  in     
-
-
-        ( { defbis with Ast_c.f_body = List.map (function s -> Ast_c.StmtElem s)
-            []
-          } , ii )
-
-    |    _ -> failwith "Impossible1"
-
-
-let analyze_toplevel file  all_fn x  =
-    match x with
-        Ast_c.Declaration decl ->  x
-    |   Ast_c.Definition def -> Ast_c.Definition (analyze_def file all_fn def)
-    |   Ast_c.CppTop directive ->  x
-    |   Ast_c.MacroTop (s, es,   [i1;i2;i3;i4]) -> x
-    |   Ast_c.EmptyDef ii ->  x
-    |   Ast_c.NotParsedCorrectly ii -> x
-    |   Ast_c.FinalDef info -> x
-    |   Ast_c.IfdefTop ifdefdir -> x
-    |   Ast_c.MacroTop _ -> x
+let analyze_toplevel file all_fn x =
+  match x with
+  | Ast_c.Definition def -> Ast_c.Definition (analyze_def file all_fn def)
+  | Ast_c.Declaration _
+  | Ast_c.CppTop _
+  | Ast_c.EmptyDef _
+  | Ast_c.NotParsedCorrectly _
+  | Ast_c.FinalDef _
+  | Ast_c.IfdefTop _
+  | Ast_c.MacroTop _ -> x
--- main.ml
+++ main.ml
@@ -22,58 +22,49 @@
 open Common
 
 
-let rec collec_all_fn = function
-  []-> []
-  | h::t-> 
-    match h with
-         Ast_c.Declaration decl -> collec_all_fn t
-    |    Ast_c.Definition def -> 
-	  let defbis, ii = def in 
-           (
-              match ii with
-              | iifunc1::iifunc2::i1::i2::ifakestart::isto ->
-                let { Ast_c.f_name = name;
-                     Ast_c.f_type = (returnt, (paramst, (b, iib)));
-                     Ast_c.f_storage = sto;
-                     Ast_c.f_body = statxs;
-                     Ast_c.f_attr = attrs;
-                    }  = defbis
-                in
-	        (name,paramst,(Def.remove_stmtElelist statxs))::(collec_all_fn t)
-	      |	_ -> raise (Impossible 1)
-           )
-    |    Ast_c.CppTop directive -> collec_all_fn t
-    |    Ast_c.MacroTop (s, es,   [i1;i2;i3;i4]) ->collec_all_fn t
-    |    Ast_c.EmptyDef ii ->  collec_all_fn t
-    |    Ast_c.NotParsedCorrectly ii -> collec_all_fn t
-    |    Ast_c.FinalDef info -> collec_all_fn t
-    |    Ast_c.IfdefTop ifdefdir -> collec_all_fn t
-    |    Ast_c.MacroTop _ -> collec_all_fn t
-    |    Ast_c.Namespace _ -> failwith "namespaces not supported"
-
+let collec_all_fn = 
+  let extract_functions acc el =  
+    match el with
+      Ast_c.Definition (defbis, _) -> 
+      let { Ast_c.f_name = name;
+            Ast_c.f_type = (_, (paramst, _));
+            Ast_c.f_body = statxs;
+          }  = defbis
+      in
+      (name, paramst, (Def.remove_stmtElelist statxs))::acc
+    | Ast_c.Declaration _
+    | Ast_c.CppTop _
+    | Ast_c.EmptyDef _
+    | Ast_c.NotParsedCorrectly _
+    | Ast_c.FinalDef _
+    | Ast_c.IfdefTop _
+    | Ast_c.MacroTop _ -> acc
+    | Ast_c.Namespace _ -> failwith "namespaces not supported"
+  in
+  List.fold_left extract_functions [] 
 
 let test_type_c infile =
   let (program2, _stat) =
     Common.profile_code "parsing" (fun () -> Parse_c.parse_c_and_cpp false infile) in
   let _program2 =
-      Common.profile_code "type inference" (fun () -> 
-    program2 
-    +> Common.unzip 
-    +> (fun (program, infos) -> 
-      Type_annoter_c.annotate_program !Type_annoter_c.initial_env 
-        program +> List.map fst,
-      infos
-    )
-    +> Common.uncurry Common.zip)
+    Common.profile_code "type inference" (fun () -> 
+        program2 
+        +> Common.unzip 
+        +> (fun (program, infos) -> 
+            Type_annoter_c.annotate_program !Type_annoter_c.initial_env 
+              program +> List.map fst,
+            infos
+          )
+        +> Common.uncurry Common.zip)
   in
   let (program, infos) = Common.unzip program2 in
 
   let all_fn = Common.profile_code "collec_all_fn" (fun () -> 
-    collec_all_fn program) in
+      collec_all_fn program) in
   let rec loop = function
-    []-> []
+      []-> []
     | h::t->
-	(Analyzer.analyze_toplevel program all_fn h)::(loop t)
+      (Analyzer.analyze_toplevel program all_fn h)::(loop t)
 
   in Common.profile_code "analysis" (fun () -> loop program)
 
@@ -99,10 +90,10 @@ let compare old_file new_code cmp =
     (function fl ->
       let tmp_old = Filename.temp_file "old_error" "c" in
       Common.command2
-	(Printf.sprintf "cp %s %s; indent -linux %s" old_file tmp_old tmp_old);
+        (Printf.sprintf "cp %s %s; indent -linux %s" old_file tmp_old tmp_old);
       let (run,show) = cmp tmp_old fl in
       match Common.cmd_to_list run with
-	[] -> ()
+        [] -> ()
       | lst -> List.iter (function x -> Printf.printf "%s\n" x) (show::lst))
 
 let compare_with_indented old_file new_code = ()
@@ -114,14 +105,14 @@ let compare_with_indented old_file new_code = ()
 let compare_with_original old_file new_code =
   compare old_file new_code
     (fun tmp_old fl ->
-      ((Printf.sprintf
-	  "diff -q %s %s > /dev/null || diff -u -p %s %s"
-	  tmp_old fl old_file fl),
-       (Printf.sprintf "diff -u -p %s %s" old_file fl)))
+       ((Printf.sprintf
+           "diff -q %s %s > /dev/null || diff -u -p %s %s"
+           tmp_old fl old_file fl),
+        (Printf.sprintf "diff -u -p %s %s" old_file fl)))
 
 let cat new_code = ()
 (*  pretty_print_to_file new_code *)
- (*   (function fl -> Common.command2 (Printf.sprintf "cat %s" fl)) *)
+(*   (function fl -> Common.command2 (Printf.sprintf "cat %s" fl)) *)
 
 let diff = ref false
 let real_diff = ref false
@@ -130,8 +121,8 @@ let verbose = ref false
 let macros = ref ""
 
 let options = [
-(* if you want command line arguments, put them here:
-   "option name", operation described in man Arg, "description"; *)
+  (* if you want command line arguments, put them here:
+     "option name", operation described in man Arg, "description"; *)
   "--profile", Arg.Unit (function () -> Common.profile := Common.PALL) ,
   "   gather timing information about the main coccinelle functions";
   "-diff", Arg.Set diff,
@@ -152,48 +143,37 @@ let anonymous str = file := str
 
 let _ =
   Arg.parse options anonymous "";
- if !macros = "" then macros := ("/usr/local/lib/coccinelle/standard.h");
- Parse_c.init_defs_builtins !macros;
- (match !org_file with Some x -> Org.parse_org x | _ -> ());
- Common.print_to_stderr := !verbose;
- Flag_parsing_c.verbose_lexing := !verbose;
- Flag_parsing_c.verbose_parsing := !verbose;
- Flag_parsing_c.verbose_type := !verbose;
-
+  if !macros = "" then macros := ("/usr/local/lib/coccinelle/standard.h");
+  Parse_c.init_defs_builtins !macros;
+  (match !org_file with Some x -> Org.parse_org x | _ -> ());
+  Common.print_to_stderr := !verbose;
+  Flag_parsing_c.verbose_lexing := !verbose;
+  Flag_parsing_c.verbose_parsing := !verbose;
+  Flag_parsing_c.verbose_type := !verbose;
 
   let files =
     if Sys.is_directory !file
     then Common.cmd_to_list ("find "^ !file ^" -name \"*.[ch]\"")
-    else [!file] in
-   ( match (!diff,!real_diff) with   
-       (false,false) -> 
+    else [!file]
 
- 	Common.main_boilerplate (fun () ->  
- 	List.iter  
- 	    (function x ->   
+  in
+  (match (!diff, !real_diff) with
+     (false, false) ->
+     Common.main_boilerplate
+       (fun () -> List.iter (function x -> cat (
+            try Common.timeout_function 60 (function () -> test_type_c x)
+            with _ -> []))
+            files)
+   | (true, _) ->
+     List.iter (function x -> compare_with_indented x (test_type_c x)) files
+   | (_, true) ->
+     List.iter (function x -> compare_with_original x (test_type_c x)) files);
 
- 	          cat (    
-                         try   
-                            Common.timeout_function 60  (function () ->	test_type_c x) with   
- 	                      Common.Timeout | Lexer_c.Lexical _ | Semantic_c.Semantic _ -> []   
- 		            | Failure s    
- 			         when   (* hack... *)
- 			           Str.string_match (Str.regexp_string "lexical error") s 0 ->[]  
-                             | _-> []
-                       )    
-             ) files)    
-            
+  (*     let ic = open_in "sound_new_rep" in *)
 
-       | (true,_) ->   
-  	List.iter (function x -> compare_with_indented x (test_type_c x)) files   
-      | (_,true) ->   
- 	List.iter (function x -> compare_with_original x (test_type_c x)) files);   
-
-(*     let ic = open_in "sound_new_rep" in *)
-
-(*      try  *)
-(*        while true do *)
-(*          let line = input_line ic in  *)
-(*          test_type_c line  *)
-(*        done;  *)
-(*      with End_of_file -> close_in ic ;  *)
+  (*      try  *)
+  (*        while true do *)
+  (*          let line = input_line ic in  *)
+  (*          test_type_c line  *)
+  (*        done;  *)
+  (*      with End_of_file -> close_in ic ;  *)
